afcb8f0c3c3b28aa8e1125c6301c8bf0e55da8b8
Alexis Suarez <suarezalexis910@gmail.com>
2023-12-22 19:31:19 -0600

    avance funciones

--- d_listadb.h
+++ d_listadb.h
@@ -24,5 +24,7 @@ int is_empty(DList DLi);
 void insertAtStartDL(DList *DLi, int newElement);
 void insertAtEndDL(DList *Dli, int newElement);
 void insertAfterNodeDL(DList *DLi, int newElement, NodeDL *a);
+void modificarHorario(ListaDoble *DLi, char nombreClase[50], char nuevoNombre[50], char nuevaHoraInicio[10], char nuevaHoraFin[10], char nuevoGrupo[5], char nuevoProfesor[50]);
+void eliminarHorario(ListaDoble *DLi, char nombreClase[50]);
 
 #endif
