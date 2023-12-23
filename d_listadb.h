a67d528f5c74999dd093a5d2a109fdb000925bee
Misaki <julioperezhernandez1345@gmail.com>
2023-12-23 14:54:55 -0600

    propuesta de estrcuturas

--- d_listadb.h
+++ d_listadb.h
@@ -1,21 +1,52 @@
 #ifndef D_LISTADB_H
 #define D_LISTADB_H
-
-
+/*
+// Nodo practico
 typedef struct NodeDL{
     int e;
     struct NodeDL *next;
     struct NodeDL *prev;
-}NodeDL;
+} NodeDL;
 
 typedef struct DList{
     NodeDL *start;
-}DList;
+} DList;
+*/
 
-typedef struct RolesUsuarios{
-    char Administrador;
-    char Alumno;
-}rol;
+typedef enum {
+    ADMINISTRADOR,
+    ALUMNO
+} Rol;
+
+typedef struct {
+    char nombreusuario[50];
+    char contrasena[50];
+    char nombreCompleto[100];
+    Rol tipoUsuario;
+} Usuario;
+
+
+typedef struct {
+    char nombreClase[50];
+    char horaInicio[10];
+    char horaFin[10];
+    char grupo[5];
+    char profesor[50];
+} HorarioClase;
+
+typedef struct 
+{
+    char nombreGrupo[10];
+    Materias MateriaAsginada;
+} Grupo;
+
+typedef enum 
+{
+    /*
+    aqui van las materias de 2do semestre
+    */
+    
+} Materias;
 
 
 
